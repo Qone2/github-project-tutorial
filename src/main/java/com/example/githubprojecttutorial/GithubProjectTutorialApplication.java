118d2db716a6874283ab764c33b653637709f856
Qone2 <whrbdnjs33@naver.com>
2022-10-21 21:21:38 +0900

    first commit

---
+++ src/main/java/com/example/githubprojecttutorial/GithubProjectTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.example.githubprojecttutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubProjectTutorialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubProjectTutorialApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/githubprojecttutorial/GithubProjectTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.githubprojecttutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubProjectTutorialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
